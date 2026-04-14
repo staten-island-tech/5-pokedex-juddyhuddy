1630e2dd812d2f94b5f2fb7f7d470d695c8e7f20
juddyhuddy <justinl732@nycstudents.net>
2026-04-14 08:46:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,38 +13,39 @@
 
 
 # Add a language choice feature and print the pokemons name based on the user input
-import json
-pokedex = open("./pokedex.json", encoding="utf8")
-data = json.load(pokedex)
-print(data[0])
-x=0
-while x < 809:
-    print(data[x]["name"])
-    x += 1
-y = (input("what language?"))
-x = 0
+# import json
+# pokedex = open("./pokedex.json", encoding="utf8")
+# data = json.load(pokedex)
+# print(data[0])
+# x=0
+# while x < 809:
+#     print(data[x]["name"])
+#     x += 1
+# y = (input("what language?"))
+# x = 0
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
-while y == "english" and x < 809:
-    print(data[x]["name"]["english"])
-    x += 1
-x = 0
-# Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
-while y == "chinese" and x < 809:
-    print(data[x]["name"]["chinese"])
-    x += 1
-x = 0
-# Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
-while y == "japanese" and x < 809:
-    print(data[x]["name"]["japanese"])
-    x += 1
-x = 0
-# Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
-while y == "french" and x < 809:
-    print(data[x]["name"]["french"])
-    x += 1
+# while y == "english" and x < 809:
+#     print(data[x]["name"]["english"])
+#     x += 1
+# x = 0
+# while y == "chinese" and x < 809:
+#     print(data[x]["name"]["chinese"])
+#     x += 1
+# x = 0
+# while y == "japanese" and x < 809:
+#     print(data[x]["name"]["japanese"])
+#     x += 1
+# x = 0
+# while y == "french" and x < 809:
+#     print(data[x]["name"]["french"])
+#     x += 1
 
     
 #Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
 
 #For Leo/, help me come up with a clever final question, considering maybe showing all moves a pokemon has avaiable based on type
-
+pokemon = (input("which pokemon"))
+if pokemon == "charizard":
+    print("charizard")
+else:
+    print("pokemon not found")
